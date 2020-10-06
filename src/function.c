4c7c521c6159485cea9ba87866e6ac652e60081e
stepin105085 <69084412+stepin105085@users.noreply.github.com>
2020-10-06 22:50:35 +0530

    Update function.c

--- src/function.c
+++ src/function.c
@@ -1,51 +1,55 @@
 #include<function.h>
 
-void fibonacci_func(int fibonacci_limit)
+double perimeter_of_triangle(double a, double b, double c)
 {
-    int init_term=0, second_term=1, nextTerm, fibonacci_iterator;
-    printf("Fibonacci Series: ");
-
-    for (fibonacci_iterator = 1; fibonacci_iterator <= fibonacci_limit; ++fibonacci_iterator) {
-        printf("%d, ", init_term);
-        nextTerm = init_term + second_term;
-        init_term = second_term;
-        second_term = nextTerm;
-    }
+	double perimeter;
+	
+	perimeter = (a+b+c);
+	
+	return perimeter;
 }
 
-int factorial_func(int factorial_input)
+double area_of_triangle(double a, double b, double c)
 {
-    int factorial_value=1,factorial_iterator;
-    if (factorial_input < 0)
-        printf("Error! Factorial of a negative number doesn't exist.");
-    else {
-        for (factorial_iterator = 1; factorial_iterator <= factorial_input; ++factorial_iterator) {
-            factorial_value *= factorial_iterator;
-        }
-    }
-    return factorial_value;
+  double s, area;
+
+  s = (a+b+c)/2;
+
+  area = sqrt(s*(s-a)*(s-b)*(s-c));
+
+  return area;
 }
 
-int prime_func(int prime_input)
+int TypeOfTriangle(double a, double b, double c)
 {
-    int prime_iterator,flag=0;
-    for (prime_iterator = 2; prime_iterator <= prime_input / 2; ++prime_iterator) {
-
-        // condition for non-prime
-        if (prime_input % prime_iterator == 0) {
-            flag = 1;
-            break;
-        }
+    if(a<=0||b<=0||c<=0)
+    {
+	    return -1;
+    } 
+    
+    if(!(a+b>c && b+c>a && a+c>b))
+    {
+	    return 0;
     }
-
-    if (prime_input == 1) {
-        printf("1 is neither prime nor composite.");
+    
+    else if(a==b && b==c)
+    {
+       return 1;
     }
-    else {
-        if (flag == 0)
-            return 1;
-        else
-            return 0;
+    
+    else if(a==b || b==c || c==a)
+    {
+       return 2;
     }
-
+    
+    else if((a*a==(b*b)+(c*c)) || (b*b==(a*a)+(c*c)) || (c*c==(a*a)+(b*b)))
+    {
+       return 3;
+    }
+    
+    else
+    {
+    	return 4;
+	}
+    
 }
