5125b3d8ad3a527527b93cb48e243a117e81f99e
stepin105085 <69084412+stepin105085@users.noreply.github.com>
2020-10-06 23:41:33 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,47 +2,64 @@
 
 int main()
 {
-int choice,fibonacci_limit,factorial_input,factorial_output,prime_input,prime_output;
-printf("From the below option choose the mathematical operation you need to perform\n");
-printf("1.Fibonacci series\n");
-printf("2.Find factorial of the number\n");
-printf("3.Check whether the number is prime or not.\n");
-printf("\n Enter your choice - ");
-scanf("%d",&choice);
-switch(choice)
-    {
-        case 1:
-            printf("\n----------------------\n");
-            printf("\nEnter fibonacci limit\n");
-            scanf("%d",&fibonacci_limit);
-            fibonacci_func(fibonacci_limit);
-            break;
+  double a, b, c, area=0, perimeter=0;
+  
+  int typeoftriangle=0;
 
-        case 2:
-            printf("\n----------------------\n");
-            printf("\nEnter the number to know factorial\n");
-            scanf("%d",&factorial_input);
-            factorial_output = factorial_func(factorial_input);
-            printf("The factorial is %d",factorial_output);
-            break;
+  printf("Enter the lengths of sides of a triangle\n");
 
-        case 3:
-            printf("\n----------------------\n");
-            printf("\nEnter the number to know whether its prime or not.\n");
-            scanf("%d",&prime_input);
-            prime_output = prime_func(prime_input);
-            if(prime_output==1)
-            {
-                printf("\nIt's a prime number\n");
-            }
-            else
-            {
-                printf("\nIt's not a prime number");
-            }
-            break;
-        default:
-            printf("\nPlease choose the correct option\n");
-            break;
-    }
+  scanf("%lf%lf%lf", &a, &b, &c);
+  
+  printf("\n");
+  
+  perimeter = perimeter_of_triangle(a, b, c);
+  
+   printf("Perimeter of the triangle = %.2lf\n", perimeter);
+   
+   printf("\n");
 
+  area = area_of_triangle(a, b, c);
+
+  printf("Area of the triangle = %.2lf\n", area);
+  
+  printf("\n");
+  
+  typeoftriangle=TypeOfTriangle(a,  b, c);
+  
+  if(typeoftriangle<=0)
+  {
+  	printf("Triangle not constructed with given lengths");
+  	
+  	printf("\n");
+  }
+  
+  if(typeoftriangle==1)
+  {
+  	printf("Triangle is Equilateral");
+  	
+  	printf("\n");	
+  }
+  
+  if(typeoftriangle==2)
+  {
+  	printf("Triangle is Isosceles ");
+  	
+  	printf("\n");
+  }
+  
+  if(typeoftriangle==3)
+  {
+  	printf("Triangle is Right Angled ");
+  	
+  	printf("\n");
+  }
+  
+  if(typeoftriangle==4)
+  {
+  	printf("Triangle is Scalene");
+  	
+  	printf("\n");
+  }
+  
+  return 0;
 }
